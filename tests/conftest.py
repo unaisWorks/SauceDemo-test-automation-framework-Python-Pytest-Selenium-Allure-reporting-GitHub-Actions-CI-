6db4ae94982b337d9b366d0423ce7dd31cc13936
“unaisLearning” <unais.learning@gmail.com>
2026-05-31 16:04:54 +0530

    update headless mode only in CI flow

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,8 +19,8 @@ def driver():
     temp_profile = tempfile.mkdtemp()
     options.add_argument(f"--user-data-dir={temp_profile}")
 
-    # options.add_argument("--headless=new")
-    options.add_argument("--headless")
+    if os.getenv("CI"):
+        options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
